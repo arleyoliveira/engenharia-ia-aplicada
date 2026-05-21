638a5aef74978a4d9fdc3e8c6ac1aca03031b836
macbookpro <arley.msn@hotmail.com>
2026-05-21 08:20:36 -0300

    feat: adiciona treinamento de recomendação de compras de produtos.

--- Fundamentos_de_IA_e_LLMs_para_Programadores/modulo-03/exemplo-01-ecommerce-recomendations/src/workers/modelTrainingWorker.js
+++ Fundamentos_de_IA_e_LLMs_para_Programadores/modulo-03/exemplo-01-ecommerce-recomendations/src/workers/modelTrainingWorker.js
@@ -3,6 +3,14 @@ import { workerEvents } from '../events/constants.js';
 
 console.log('Model training worker initialized');
 let _globalCtx = {};
+let _model = {};
+
+const WEIGHTS = {
+    category: 0.4,
+    color: 0.3,
+    price: 0.2,
+    age: 0.1
+}
 
 //Normalize continuos values (price, age) to 0-1 range
 //Why? Keeps all features balanced so no on dominates training
@@ -10,9 +18,9 @@ let _globalCtx = {};
 //Example: price=129.99, minPrice=39.99, maxPrice=199.99 -> 0.56
 const normalize = (value, min, max) => (value - min) / ((max - min) || 1)
 
-function makeContext(catalog, users) {
+function makeContext(products, users) {
     const ages = users.map(u => u.age)
-    const prices = catalog.map(p => p.price)
+    const prices = products.map(p => p.price)
 
     const minAge = Math.min(...ages)
     const maxAge = Math.max(...ages)
@@ -20,16 +28,16 @@ function makeContext(catalog, users) {
     const minPrice = Math.min(...prices)
     const maxPrice = Math.max(...prices)
 
-    const colors = [...new Set(catalog.map(p => p.color))]
-    const categories = [... new Set(catalog.map(p => p.category))]
+    const colors = [...new Set(products.map(p => p.color))]
+    const categories = [... new Set(products.map(p => p.category))]
 
-    const colorsIndex = Object.entries(
+    const colorsIndex = Object.fromEntries(
         colors.map((color, index) => {
             return [color, index]
         })
     )
 
-    const categoriesIndex = Object.entries(
+    const categoriesIndex = Object.fromEntries(
         categories.map((category, index) => {
             return [category, index]
         })
@@ -49,7 +57,7 @@ function makeContext(catalog, users) {
     });
 
     const productAvgAgeNorm = Object.fromEntries(
-        catalog.map(product => {
+        products.map(product => {
             const avg = ageCounts[product.name]
                 ? ageSums[product.name] / ageCounts[product.name]
                 : midAge
@@ -59,7 +67,7 @@ function makeContext(catalog, users) {
     )
 
     return {
-        catalog,
+        products,
         users,
         colorsIndex,
         categoriesIndex,
@@ -69,8 +77,145 @@ function makeContext(catalog, users) {
         maxPrice,
         numCategories: categories.length,
         numColors: colors.length,
-        dimentions: 2 + categories.length + colors.length //price + age + colors + categories
+        dimentions: 2 + categories.length + colors.length, //price + age + colors + categories
+        productAvgAgeNorm
     }
+}
+
+const oneHotWeighted = (index, length, weight) => tf.oneHot(index, length).cast('float32').mul(weight)
+
+function encodeProduct(product, context) {
+    //Normalizando dados para ficar de 0 a 1
+    //aplicação peso na recomendação
+    const price = tf.tensor1d([
+        normalize(
+            product.price,
+            context.minPrice,
+            context.maxPrice
+        ) * WEIGHTS.price
+    ])
+
+    const age = tf.tensor1d([
+        (
+            context.productAvgAgeNorm[product.name] ?? 0.5
+        ) * WEIGHTS.age
+    ])
+
+    const category = oneHotWeighted(
+        context.categoriesIndex[product.category],
+        context.numCategories,
+        WEIGHTS.category
+    )
+
+    const color = oneHotWeighted(
+        context.colorsIndex[product.color],
+        context.numColors,
+        WEIGHTS.color
+    )
+    return tf.concat1d(
+        [
+            price,
+            age,
+            category,
+            color
+        ]
+    )
+}
+function encodedUser(user, context) {
+    if (user.purchases.length) {
+        return tf.stack(
+            user.purchases.map(product => encodeProduct(product, context))
+        )
+            .mean(0)
+            .reshape([
+                1,
+                context.dimentions
+            ])
+    }
+}
+
+function createTrainingData(context) {
+    const inputs = []
+    const labels = []
+    context.users
+        .filter(u => u.purchases.length)
+        .forEach(user => {
+            const userVector = encodedUser(user, context).dataSync()
+            context.products.forEach(product => {
+                const productVector = encodeProduct(product, context).dataSync()
+
+                const label = user.purchases.some(
+                    purchase => purchase.name === product.name ? 1 : 0
+                )
+
+                //combinar user + product
+                inputs.push([...userVector, ...productVector])
+                labels.push(label)
+            })
+        })
+    return {
+        xs: tf.tensor2d(inputs),
+        ys: tf.tensor2d(labels, [labels.length, 1]),
+        inputDimention: context.dimentions * 2 //tamanho = userVector + productVector
+    }
+}
+
+
+//Configuração e treinamento da rede neural
+async function configureNeuralNetAndTrain(trainData) {
+    const model = tf.sequential()
+
+    model.add(
+        tf.layers.dense({
+            inputShape: [trainData.inputDimention],
+            units: 128, //experimental (muitos olhos para detectar padrões)
+            activation: 'relu' // mantém apenas sinais positivos, ajuda a aprender padrões não lineares
+        })
+    )
+
+    //camadas serve para aprender mais, uma vez que serão removido os negativos, como se fosse uma espécie de filtro
+    model.add(
+        tf.layers.dense({
+            units: 64, //experimental (muitos olhos para detectar padrões)
+            activation: 'relu' // mantém apenas sinais positivos, ajuda a aprender padrões não lineares
+        })
+    )
+    model.add(
+        tf.layers.dense({
+            units: 32, //experimental (muitos olhos para detectar padrões)
+            activation: 'relu' // mantém apenas sinais positivos, ajuda a aprender padrões não lineares
+        })
+    )
+
+    //camada de saída
+    model.add(
+        tf.layers.dense({
+            units: 1, //somente um dado, pois quero saber apenas da propabilidade que o usuário tem para comprar determinado produto
+            activation: 'sigmoid' //ordena com a lista com da maior para menor probabilidade
+        })
+    )
+
+    model.compile({
+        optimizer: tf.train.adam(0.01),
+        loss: 'binaryCrossentropy',
+        metrics: ['accuracy']
+    })
+
+    await model.fit(trainData.xs, trainData.ys, {
+        epochs: 100, //quantidade que o modelo irá rodar com a base de dados
+        batchSize: 32, //quantidade de dados por vez que serão treinados
+        shuffle: true,
+        callbacks: {
+            onEpochEnd: (epoch, logs) => {
+                postMessage({
+                    type: workerEvents.trainingLog,
+                    epoch: epoch,
+                    loss: logs.loss,
+                    accuracy: logs.acc
+                });
+            }
+        }
+    })
 }
 
 async function trainModel({ users }) {
@@ -78,21 +223,25 @@ async function trainModel({ users }) {
 
     postMessage({ type: workerEvents.progressUpdate, progress: { progress: 50 } });
 
-    const catalog = await (await fetch('/data/products.json')).json()
+    const products = await (await fetch('/data/products.json')).json()
 
-    const context = makeContext(catalog, users)
-    debugger
-    postMessage({
-        type: workerEvents.trainingLog,
-        epoch: 1,
-        loss: 1,
-        accuracy: 1
-    });
+    const context = makeContext(products, users)
 
-    setTimeout(() => {
-        postMessage({ type: workerEvents.progressUpdate, progress: { progress: 100 } });
-        postMessage({ type: workerEvents.trainingComplete });
-    }, 1000);
+    context.productVectors = products.map(product => {
+        return {
+            name: product.name,
+            meta: { ...product },
+            vector: encodeProduct(product, context).dataSync()
+        }
+    })
+
+    _globalCtx = context
+
+    const trainData = createTrainingData(context)
+    _model = await configureNeuralNetAndTrain(trainData)
+
+    postMessage({ type: workerEvents.progressUpdate, progress: { progress: 100 } });
+    postMessage({ type: workerEvents.trainingComplete });
 
 
 }
