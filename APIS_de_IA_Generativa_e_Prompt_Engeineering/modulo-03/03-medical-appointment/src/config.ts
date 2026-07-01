374bfc31af2024593087e2568d26f03b8f4b3a66
macbookpro <arley.msn@hotmail.com>
2026-07-01 09:04:10 -0300

    feat: adiciona exemplo de marcação e cancelamento de consulta médica com ia usando o open router, langchain e langsmith

---
+++ APIS_de_IA_Generativa_e_Prompt_Engeineering/modulo-03/03-medical-appointment/src/config.ts
@@ -0,0 +1,36 @@
+export type ModelConfig = {
+  apiKey: string;
+  httpReferer: string;
+  xTitle: string;
+
+  provider: {
+    sort: {
+      by: string;
+      partition: string;
+    };
+  };
+
+  models: string[];
+  temperature: number;
+};
+
+console.assert(process.env.OPENROUTER_API_KEY, 'OPENROUTER_API_KEY is not set in environment variables');
+
+export const config: ModelConfig = {
+  apiKey: process.env.OPENROUTER_API_KEY!,
+  httpReferer: '',
+  xTitle: 'IA Devs - Prompt Chaining Article Generator',
+  models: [
+    // 'qwen/qwen3-coder-next',
+    // https://openrouter.ai/models?fmt=cards&max_price=0&order=throughput-high-to-low&supported_parameters=structured_outputs%2Cresponse_format
+    'google/gemma-4-26b-a4b-it:free',
+    //'arcee-ai/trinity-large-preview:free',
+  ],
+  provider: {
+    sort: {
+      by: 'throughput', // Route to model with highest throughput (fastest response)
+      partition: 'none',
+    },
+  },
+  temperature: 0.7,
+};
---
+++ APIS_de_IA_Generativa_e_Prompt_Engeineering/modulo-03/03-medical-appointment/src/graph/nodes/messageGeneratorNode.ts
@@ -0,0 +1,59 @@
+import { getSystemPrompt, getUserPromptTemplate, MessageSchema } from '../../prompts/v1/messageGenerator.ts';
+import { OpenRouterService } from '../../services/openRouterService.ts';
+import type { GraphState } from '../graph.ts';
+import { AIMessage } from 'langchain';
+
+export function createMessageGeneratorNode(llmClient: OpenRouterService) {
+    return async (state: GraphState): Promise<Partial<GraphState>> => {
+        console.log(`💬 Generating response message...`);
+
+        try {
+
+            const hasSucceeded = state.actionSuccess === true ? 'success' : 'error';
+            const scenario = `${state.intent ?? 'unknown'}+${hasSucceeded}`;
+            const datails = {
+                professionalName: state.professionalName,
+                dateTime: state.datetime,
+                patientName: state.patientName,
+                error: state.error,
+            }
+
+            const systemPrompt = getSystemPrompt();
+            const userPrompt = getUserPromptTemplate({ scenario, details: datails });
+
+            const result = await llmClient.generateStructured(
+                systemPrompt,
+                userPrompt,
+                MessageSchema
+            );
+            console.log(`✅ Message generated:`, result.data?.message ?? result.data ?? result);
+
+            if(result.error)  {
+                console.log(`⚠️  Message generation failed: ${result.error}`);
+                return {
+
+                    messages: [
+                        ...state.messages,
+                        new AIMessage("Desculpe, errei!")
+                    ],
+                };
+            }
+
+            return {
+                messages: [
+                    ...state.messages,
+                    new AIMessage(result.data!.message)
+                ],
+            };
+        } catch (error) {
+            console.error('❌ Error in messageGenerator node:', error);
+            return {
+                ...state,
+                messages: [
+                    ...state.messages,
+                    new AIMessage('An error occurred while processing your request.')
+                ],
+            };
+        }
+    };
+}
